5e16637ba816e6396e95be2c660a9d2bb3fd6536
ichikawa7ss <i2s7.chacha@gmail.com>
2022-12-23 12:56:41 +0900

    Make project

---
+++ ImageCacheLibraryResearch/ImageCacheLibraryResearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCacheLibraryResearchApp.swift
+//  ImageCacheLibraryResearch
+//
+//  Created by ichikawa on 2022/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageCacheLibraryResearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
