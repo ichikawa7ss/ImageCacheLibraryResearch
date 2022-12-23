f8eb0a96d4890bace39f769e038e8b2a8202c867
ichikawa7ss <i2s7.chacha@gmail.com>
2022-12-23 17:39:48 +0900

    Implement NukeView.

--- ImageCacheLibraryResearch/Sources/NukeView.swift
+++ ImageCacheLibraryResearch/Sources/NukeView.swift
@@ -6,15 +6,34 @@
 //
 
 import SwiftUI
+import NukeUI
 
 struct NukeView: View {
-    var body: some View {
-        Text("Nuke")
+  let itemWidth: Int
+  let itemHeight: Int
+  
+  init() {
+    let width = Int(UIScreen.main.bounds.width / 3)
+    print(width)
+    self.itemWidth = width
+    self.itemHeight = width * 2
+  }
+  
+  var body: some View {
+    ScrollView {
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 200))]) {
+        ForEach((1...50), id: \.self) { index in
+          let url = "https://picsum.photos/id/\(index)/\(String(itemWidth))/\(String(itemHeight))"
+          LazyImage(url: .init(string: url))
+            .frame(width: CGFloat(itemWidth), height: CGFloat(itemHeight))
+        }
+      }
     }
+  }
 }
 
 struct NukeView_Previews: PreviewProvider {
-    static var previews: some View {
-        NukeView()
-    }
+  static var previews: some View {
+    NukeView()
+  }
 }
