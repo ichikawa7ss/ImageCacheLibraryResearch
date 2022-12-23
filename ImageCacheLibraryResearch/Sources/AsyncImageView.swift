350812644c70698d800207cb5f24bd6298046cee
ichikawa7ss <i2s7.chacha@gmail.com>
2022-12-23 16:35:02 +0900

    Implement grid layout with AsyncImage.

--- ImageCacheLibraryResearch/Sources/AsyncImageView.swift
+++ ImageCacheLibraryResearch/Sources/AsyncImageView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct AsyncImageView: View {
-    var body: some View {
-        Text("AsyncImage")
+  
+  let itemWidth: Int
+  let itemHeight: Int
+  
+  init() {
+    let width = Int(UIScreen.main.bounds.width / 3)
+    print(width)
+    self.itemWidth = width
+    self.itemHeight = width * 2
+  }
+  
+  var body: some View {
+    ScrollView {
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 200))]) {
+        ForEach((1...50), id: \.self) { index in
+          let url = "https://picsum.photos/id/\(index)/\(String(itemWidth))/\(String(itemHeight))"
+          AsyncImage(url: .init(string: url))
+            .frame(width: CGFloat(itemWidth), height: CGFloat(itemHeight))
+            .onAppear {
+              print("[AsyncImage] index: \(index), url: \(url)")
+            }
+        }
+      }
     }
+  }
 }
 
 struct AsyncImageView_Previews: PreviewProvider {
-    static var previews: some View {
-        AsyncImageView()
-    }
+  static var previews: some View {
+    AsyncImageView()
+  }
 }
