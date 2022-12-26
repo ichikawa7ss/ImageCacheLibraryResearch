ef0f41da878501f573b6167381da6f0e2cd25194
ichikawa7ss <i2s7.chacha@gmail.com>
2022-12-26 12:08:21 +0900

    Implement SDWebImageView.

--- ImageCacheLibraryResearch/Sources/SDWebImageView.swift
+++ ImageCacheLibraryResearch/Sources/SDWebImageView.swift
@@ -6,15 +6,35 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct SDWebImageView: View {
-    var body: some View {
-        Text("SDWebImage")
+  let itemWidth: Int
+  let itemHeight: Int
+  
+  init() {
+    let width = Int(UIScreen.main.bounds.width / 3)
+    print(width)
+    self.itemWidth = width
+    self.itemHeight = width * 2
+  }
+  
+  var body: some View {
+    ScrollView {
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 200))]) {
+        ForEach((1...50), id: \.self) { index in
+          let url = "https://picsum.photos/id/\(index)/\(String(itemWidth))/\(String(itemHeight))"
+          WebImage(url: .init(string: url), options: SDWebImageOptions.allowInvalidSSLCertificates, context: [
+          ])
+            .frame(width: CGFloat(itemWidth), height: CGFloat(itemHeight))
+        }
+      }
     }
+  }
 }
 
 struct SDWebImageView_Previews: PreviewProvider {
-    static var previews: some View {
-        SDWebImageView()
-    }
+  static var previews: some View {
+    SDWebImageView()
+  }
 }
