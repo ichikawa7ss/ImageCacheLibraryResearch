2d75cea3244c7f07d49288b0770a7f88e156c773
ichikawa7ss <i2s7.chacha@gmail.com>
2023-01-27 13:56:28 +0900

    Use Kingfisher.

--- ImageCacheLibraryResearch/Sources/KingfisherView.swift
+++ ImageCacheLibraryResearch/Sources/KingfisherView.swift
@@ -5,12 +5,32 @@
 //  Created by ichikawa on 2022/12/23.
 //
 
+import Kingfisher
 import SwiftUI
 
 struct KingfisherView: View {
-    var body: some View {
-        Text("Kingfisher")
+  
+  let itemWidth: Int
+  let itemHeight: Int
+  
+  init() {
+    let width = Int(UIScreen.main.bounds.width / 3)
+    print(width)
+    self.itemWidth = width
+    self.itemHeight = width * 2
+  }
+  
+  var body: some View {
+    ScrollView {
+      LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 200))]) {
+        ForEach((1...50), id: \.self) { index in
+          let url = "https://picsum.photos/id/\(index)/\(String(itemWidth))/\(String(itemHeight))"
+          KFImage(.init(string: url))
+            .frame(width: CGFloat(itemWidth), height: CGFloat(itemHeight))
+        }
+      }
     }
+  }
 }
 
 struct KingfisherView_Previews: PreviewProvider {
